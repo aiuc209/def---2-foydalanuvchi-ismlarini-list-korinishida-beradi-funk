2a3462bc4d38c66b8411a6e48bf0fe0668e5b97c
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:29:21 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Def   2 Foydalanuvchi Ismlarini List Korinishida Beradi Funk
+Topic: DEF - 2: �� Foydalanuvchi ismlarini list ko‘rinishida beradi. Funksiya har bir ismni uzunligiga qarab quyidagicha  tasniflaydi:
+"""
+
+def main():
+    print("Welcome to Def   2 Foydalanuvchi Ismlarini List Korinishida Beradi Funk!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
